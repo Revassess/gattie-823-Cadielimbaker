3a04a182edea1285ae1083aa7ce50275211fb516
Cadie Limbaker <cadielimbaker@gmail.com>
2020-08-26 14:29:27 -0400

    testing 1

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,11 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
+    	
+    	String string1 = "hello";
+    	String string2 = "goodbye";
+    	
+    	System.out.println(string1.compareTo(string2));
         return false;
     }
 }
