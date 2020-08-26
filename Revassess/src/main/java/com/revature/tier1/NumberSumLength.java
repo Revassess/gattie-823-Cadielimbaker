f359793bf77df15cceefcc5a8ceeec2e3cecfb29
Cadie Limbaker <cadielimbaker@gmail.com>
2020-08-26 16:32:47 -0400

    attempt at sumArray

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,9 +1,20 @@
 package com.revature.tier1;
 
+//import java.util.stream.Stream;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		//long length = Stream.of(num).count();
+		int length = String.valueOf(num).length();
+		Math.pow(num, length);
+		long sum = length + length;
 		
+		if(num==sum) {
+		System.out.println("the sum of powers(length of integer) is equal to the original integer");
+		return true;
+	}else {
 		return false;
-	}
+}
+}//don't know if I can use Math.pow because of doubles
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -1,8 +1,29 @@
 package com.revature.tier1;
 
+import java.util.Scanner;
+
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+		int[] input = new int[length];
+		
+		System.out.println("enter the elements of the array");
+		for(int i = 0; i < length; i++) {
+			input[i]=sc.nextInt();
+		}
+		int total = sumOfElements(input);
+		System.out.println("Sum of all the array elements is:" + total);
+		sc.close();
+		return total;
+	}
+
+	private static int sumOfElements(int[] arr) {
+		int sum = 0;
+		for(int i : arr) {
+			sum = sum + i;
+		}
+		return sum;
 	}
 }
